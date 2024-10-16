7e7cd956056c4f18cdb601265419a77f30d5a621
Ketki <davdaketki@gmail.com>
2024-10-16 18:08:57 +0530

    Update Essays.js

--- src/components/Essays.js
+++ src/components/Essays.js
@@ -6,13 +6,32 @@ export const essays = [
 		    image: "images/stones.png", // Replace with the actual image path
 
         content: `
-            This is the first paragraph of Essay One. It contains some introductory text.
+            Second entry to my anxiety journals drumroll. It's only fitting that I write about the worst few days of my life ever. Oh to be young and foolish! Thank God for big girl folly! (You can find the first entry here.)
 
-            This is the second paragraph. It continues the discussion and provides more details.
+This is a story from back when I was 19. The year was 2018. The month was December. I had been reading a lot of eat pray love kinda literature and decided that the time was ripe for me to meditate. The last few years before that had been horribly mercurial. I used to start crying in the middle of lectures. I would stay up all night agonizing over a small conversation. Things were not great! I decided that until I could afford therapy with big girl money, I might as well try vipassana meditation!
 
-            Here comes the third paragraph. It might offer some concluding thoughts or reflections.
+Thanks for reading kets99’s Substack! Subscribe for free to receive new posts and support my work.
 
-            And finally, this is the fourth paragraph, wrapping up Essay One.
+
+Subscribed
+
+So it was decided. I packed my bags with comfy clothes and sweaters to stay at the vipassana center for 11 days. No talking. No technology. Only me and my mind, trying to focus. Trying to meditate using only my breath for company.
+
+Looking back, this seems like such an idiotic decision from the get-go. Oh! You’ve been an anxious mess since you were a child, why don't you go try meditating in an unregulated environment.
+
+Starting with the first few days, they were going GREAT! We would wake up at 4:30. Meditate for a bit, a great breakfast, shower, and I had a spot where I watched the sunrise everyday. It was quiet, peaceful, the food was simple and delicious. No complaints really!
+
+The trouble started 6/7 days in. The days had started all merging into one and a few people had given up and gone back home too! Over the last few years, I've realized that I have mood cycles resembling a wave. So it goes up and down very naturally. When that happens in regular life, there is bound to be something external pissing me off at the same time, so I attribute that sad wave cycle to the thing. But here, there was nothing bothering me.
+
+So I assumed I had gone mad. This is a very natural assumption to make when you are going into endless thought spirals, audibly crying in the middle of a meditation session and looking around for things to kill yourself with. It really did seem like the end of the world. I was 19 and fully convinced that I was going to be a sad girl for the rest of my life. That this fog was never going to lift. And not having anyone to share my worries with, not even a journal; only exacerbated these thoughts.
+
+I would zone out during my meditations, this was natural. But I went from thinking about beaches and holidays to picturing a sad me in college in the next semester. Picturing a sad me on holiday. At the beach. My little self at 19, forever destined to be sad from now on. If this seems laughable and ridiculous now, it was not at the time. It was serious business. In fact, a lot of my spiraling came from thinking about future me. About how I had broken myself by coming here and I would remain broken.
+
+Simple meditations that were usually a breeze were now impossible for me. Everytime I entered the hall for a 2 hour long session, I would be in cold shivers over what fresh hell my imagination would unleash on me. And unlike cases where the imagined is usually worse than reality, reality was worse here. Short 2 hour meditations were terrifying. 
+
+It was many years back, but I remember that I got myself exempt from one of the evening 430-630 sessions and just sat in the common verandah. Pretty sure they looked at my stricken, red face and thought, sure take a break. You probably deserve it! So I did! Except that the time off did not really help and I was wandering around the property, looking at things, thinking of how I was going to live with this sadness forever. In hindsight, I was way over reacting to a simple mood swing.
+
+But if you have been 19 and anxious, you know what I’m talking about. You know how the days stretch endlessly and painfully. You know how every attempt at expressing yourself results in a jumble of strained, throaty crying. Thankfully, we were nearing the end of the 10 days and talking restrictions had been lifted. And my mood swing was coming back to normal too. Everyone was now saying hi, introducing themselves and sharing their life stories. I think my mood was still a little subdued and shocked. The entire thing resembled a feverish nightmare. Every recollection and retelling of it still feels so surreal and otherworldly.
         `,
     },
     {
