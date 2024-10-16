f1ef8c87a336452ece35e0ef9a83f4e417afaf44
Ketki <davdaketki@gmail.com>
2024-10-16 18:13:03 +0530

    Update Essays.js

--- src/components/Essays.js
+++ src/components/Essays.js
@@ -40,13 +40,25 @@ But if you have been 19 and anxious, you know what I’m talking about. You know
 				    image: "images/sleepless.png", // Replace with the actual image path
 
         content: `
-            This is the first paragraph of Essay Two. It contains different introductory text.
+            Third Entry!
 
-            This is the second paragraph of Essay Two. It elaborates on another topic.
+This story is from July 2019. The start of my 4th year of engineering college. Placement season. I was 20 years old. Everyone in my class was frantically applying to companies. Placement is a multistep process. Each more frustrating and tedious than the last. The steps for each company went like this: Fill out a form, upload all your documents, reach college early in the morning, give a 1.5-hour aptitude test with questions ranging from math and English to coding, wait for their results. If selected, go through a series of rounds and interviews comprising of group discussions, online video interviews, interviews in college (I once waited for an interview at 5 pm from 8 am since they had to get through other candidates before me), interviews out of town and so on. A torturous, almost dehumanising process.
 
-            Third paragraph discussing further points.
+For one of these companies, after we cleared the aptitude, coding and online interview round, they wanted to drive us out to Pune from Mumbai for interviews. They had set up interviews with kids from other colleges in Pune and this would be more convenient for them. The company had arranged cars to and from our college itself. Obviously, the idea to refuse or bargain did not occur to anyone. When I look back at the time, we were treating it like each single interview, every single test was your ride or die. I was constantly surrounded by this energy and began to emulate it even. So Pune, it was.
 
-            Finally, concluding thoughts on Essay Two.
+We left by 430 pm and reached the ibis by 8 pm, amidst maddening rains. My system was already on high anxiety due to sleeplessness and jitters the night before.
+
+I was also traveling in the same car as my former best friend, we’d had a horrible falling apart that year and being around her hurt. It physically hurt. We shared a lot of history and even a small conversation would send my heart into overdrive. I was so worried about saying something rude or inadvertently being mean. Not that I cared about her feelings, make no mistake, I simply did not want to get into an argument again. I did not want to burden my stomach with any more anxiety gurgles than necessary. It was already running at capacity.
+
+Next, I was sharing my room with a stranger, and we had planned to switch it with another person we knew. But turns out, the other person despised my stranger roommate so we were stuck together. Honestly, the roommate wasn’t even that bad. When I actually met her, she was nice and normal. I suppose the other girl and her had some sore history too.
+
+All of this combined was getting too much to bear. I was low on sleep, high on worry and the ever-looming belief that I was simply wasting everyone's time would not leave me. It felt like the tiny room with twin beds was closing in on me. A friend came to hang out with me and we ordered shawarmas while watching a film on tv. It was probably 9 pm and the evening felt surreal. What was I doing? Cosplaying a grown up who belonged here in a fancy hotel room eating takeout and watching films? Had I come all this way to be worried, bomb an interview and waste everyone’s money. The evening was such a heavy bag of emotions. Shame, anxiety, panic, omniscient sleeplessness.
+
+Once my friend was gone, I was alone in my bed with ample time to go through the events of the day. I thought of all the fights I had had with my best friend, leading us to this impasse. I thought of every single interview I had messed up this year, every single time I did not take the correct tone of voice or let my worries show. Every second I showed them my authentic, worried self as they sat across the table and decided to reject me. It was an intense night as I lay there desperately praying to the gods I didn’t believe in, begging them to put me to sleep, to put me out of my misery. 3 am turned to 4 which turned to 5. I was counting backwards trying to determine how many hours of sleep I would get if I fell asleep now, immediately.
+
+I thought of my parents, how they had supported me through everything, and would support me even now. Even after I failed this interview and went back home tomorrow, they’d hug me and pat my head as I tried to sleep. I longed to have them near me now, to hear something that would assuage my anxiety. I also felt guilty for wasting their money traveling to Pune when I knew in my gut, I would probably not get the job. If I sucked so much at interviews, why was I doing them?
+
+Sometime after 5 am, my brain was tired of catastrophizing and fell asleep. I woke up at 8 am with puffy eyes, got dressed in formals and left the hotel. The entire day was a mishmash worry soup of anxiety and panic but that’s for another essay. Needless to say, I bombed this interview as well, went back home from college crying in a crowded local train and sat in my parents' room begging them to take me to a mental health doctor between sobs.
         `,
     },
 {
@@ -55,13 +67,45 @@ But if you have been 19 and anxious, you know what I’m talking about. You know
 				    image: "images/exam.png", // Replace with the actual image path
 
         content: `
-            This is the first paragraph of Essay Two. It contains different introductory text.
+            hi! I recently turned 25 and my mental health has been 🚀. It’s on an upward spiral BABY!
 
-            This is the second paragraph of Essay Two. It elaborates on another topic.
+Over the years, I have suffered through debilitating, soul-crushing anxiety, but we’re finally seeing some light. It’s still not the end of the tunnel though. I’m still on my meds, I still have occasional nights where everything falls apart. But its good. Overall.
 
-            Third paragraph discussing further points.
+I’m so grateful to my brain for finally calming the fuck down and for scientists and pharmacists working on medicines and my doctor for prescribing them to me and my friends and family for being so kind and supportive and patient with me.
 
-            Finally, concluding thoughts on Essay Two.
+What an incredible ride and its far from over! Hence: this blog. Sometimes when I’m feeling down, it helps to think of all the previous times I overcame this. That’s why I want to record it, to have proof of posterity. Look how far you’ve come! The second is commiseration. Maybe someday a young person can read this and think “oh thank god I'm not alone!”. That’s how I felt when I first read Sylvia Plath. That’s how I felt when I discovered Allison Raskin (YouTube, Just Between Us). She was anxious and had OCD and was talking about her social anxiety and not fitting in and 17-year-old me felt seen for the first time ever.
+
+It’s so important to know you’re not alone. That many have walked this path before you and come out the other side. I hope this blog can be that space!
+
+My earliest memory of being anxious, having a debilitating anxiety attack actually, is from when I was 10.
+
+I’m sure there were times before this, but nothing comes to mind. I remember other childhood instances of feeling isolated socially, feeling scared/cranky/nervous, but nothing that can be explicitly tagged as anxiety or panic.
+
+Back to the scene. I was 10. Newly entering 6th grade and an awkward, shy kid. My main source of friends was this dance group I was in. Everyone in the group was participating in this math exam called IPM, taking weekly classes for it. Desi peer pressure, so obviously I signed up too. On the exam day, I showed up nervous and made my way to the designated seat.
+
+They handed us the paper and my hands were clammy just grabbing it. It was a big fat booklet with 100 questions. Combine that with an OMR answer sheet and everyone warning you to not mark things incorrectly, to be cautious because there would be no extra sheet. Everything was overwhelming.
+
+We had gone through months of rigorous training for this test and I was freaking out on test day. The realization only scared me more. I couldn't stop getting into my head, looking at simple questions and gulping nervously.
+
+Questions that would have been stupidly obvious were confusing me. Is 2x2 4? Or is it 8? THINK!!
+
+The confusing, scary mood of the exam hall did not help either. The invigilator was a middle-aged woman who had just called a kid off for talking. Nothing extreme as far as Indian teachers go. She was probably annoyed at having to come in on a Sunday too. But I was petrified.
+
+Now, not only was I failing the test, but now this woman was going to hate me as well. And maybe scream at me! And judge me for not making any calculations and just sitting there looking scared.
+
+My tiny self was going through a lot of emotions! Fear of failing the test. Fear of disappointing my parents. Fear of being ridiculed. Fear of the invigilator judging me! All of this combined was too much for my brain to handle. It was running overtime with fear.
+
+Suddenly I started feeling cold. This was very weird since it was a Mumbai summer. I started rubbing my arms to feel better. Then I thought: You're making this up so that you won't be held responsible for the results of this test. And because you don't want the invigilator to think you're stupid. You’d rather she think you’re sick today.
+
+Yes, this is all a drama. So, I decided to keep up the charade. Suddenly my head was hurting, and I was feeling weak. A few minutes later I was crying quietly too! The invigilator came up to me and was suddenly so kind and thoughtful. She asked me what was going on. I told her I didn't feel great. She got me some water and a tablet. I was so convinced that this was going to make me sicker since I wasn't actually sick.
+
+I spent the rest of the time in the exam hall crying quietly and making up scenarios where I’d go out and tell my mother I had fallen sick, and she’d console me and give me hugs. She would tell me it was okay. And when the results came out, we would remember this day and my sickness. Yes, Kets is still good at math, she was just sick that day.
+
+There was also the guilt of pretending to fall sick. Thankfully no one was able to see through my charade. I was breaking into cold sweats and my face surely looked sickly.
+
+I held this secret for many years, until I recently looked back and realized that I’d had an anxiety attack. I learnt that panic, nervousness, weird feeling in my tummy, suddenly feeling super cold, wanting to lie on the cold floor before exams were all symptoms of an actual problem and not me being a lazy weakling. This was always the narrative I’d been fed.
+
+Every time I look back at my childhood and think of the anxious and scared kid, I send a prayer to the universe as a thank you for adulthood. 
         `,
     },{
         id: 4,
