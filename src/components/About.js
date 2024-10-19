9fa331ee66c3946e184e39a1dd143f483e856012
Ketki <davdaketki@gmail.com>
2024-10-19 14:57:44 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className="about" style={{ padding: '20px' }}>
             <h2>about us</h2>
             <p>an online space for anxious individuals to share their <br/> stories <br/> i think there is great power in commiseration, <br/> just knowing that someone before you went through  <br/>  the same stuff and came out stronger the other side!</p>
-				<p>we're trying to compile stories and experiences of people's struggle with anxiety <br/> for more questions, please write to us at ophelia2999@gmail.com </p>
+				<p>we're trying to compile stories and experiences of people's struggle with anxiety <br/> for submissions/questions, please write to us at ophelia2999@gmail.com </p>
 			
 			
 			</div>
