93cbc36ba9b32a1c605323088064e12b7a8a991e
Ketki <davdaketki@gmail.com>
2024-10-30 19:37:36 +0530

    Update Box.js

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Box.css'; // Create a CSS file for styling
 
-const Box = ({ title, image }) => {
+const Box = ({ title, image, link }) => {
   return (
     <div className="box">
-	      <h2 className="box-title">{title}</h2>
-      <img src={image} alt={title} className="box-image" />
+      {image && <img src={image} alt={title} className="box-image" />}
+      <h2 className="box-title">
+        <a href={link}>{title}</a>
+      </h2>
     </div>
   );
 };
